04bef24884771215637b9687200ce239e5bbc94b
Felipe Burgos <hhh.hb38@gmail.com>
2022-06-03 17:29:16 -0300

    Impementacion de la API

--- src/main/java/com/propiedades/alquiler/controller/AlquileresController.java
+++ src/main/java/com/propiedades/alquiler/controller/AlquileresController.java
@@ -1,7 +1,35 @@
 
 package com.propiedades.alquiler.controller;
 
+import com.propiedades.alquiler.dto.PropiedadDTO;
+import com.propiedades.alquiler.model.Inquilino;
+import com.propiedades.alquiler.model.Propiedad;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class AlquileresController {
     
+    @GetMapping("/propiedad/{id}")
+    @ResponseBody
+    public PropiedadDTO devolverPropiedad(@PathVariable Long id){
+        
+        Inquilino inqui = new Inquilino(1L, "12345678", "Walter", "white", "Porfesor");
+        Propiedad prop = new Propiedad(152365L, "Casa", "308 Negra Arroyo Lane ", 200.0, 400000.0);
+        
+        
+        PropiedadDTO propiDTO = new PropiedadDTO();
+        
+        propiDTO.setId_propiedad(prop.getId_propiedad());
+        propiDTO.setTipo(prop.getTipo_propiedad());
+        propiDTO.setDireccion(prop.getDireccion());
+        propiDTO.setValor_alquiler(prop.getValor_alquiler());
+        propiDTO.setNombre_inquilino(inqui.getNombre());
+        propiDTO.setApellido_inquilino(inqui.getApellido());
+        
+        return propiDTO;
+        
+    }
 }
--- src/main/java/com/propiedades/alquiler/dto/PropiedadDTO.java
+++ src/main/java/com/propiedades/alquiler/dto/PropiedadDTO.java
@@ -1,6 +1,62 @@
-
 package com.propiedades.alquiler.dto;
 
-public class PropiedadDTO {
-    
+import java.io.Serializable;
+
+public class PropiedadDTO implements Serializable {
+
+    private Long id_propiedad;
+    private String tipo;
+    private String direccion;
+    private Double valor_alquiler;
+    private String nombre_inquilino;
+    private String apellido_inquilino;
+
+    public void setId_propiedad(Long id_propiedad) {
+        this.id_propiedad = id_propiedad;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public void setValor_alquiler(Double valor_alquiler) {
+        this.valor_alquiler = valor_alquiler;
+    }
+
+    public void setNombre_inquilino(String nombre_inquilino) {
+        this.nombre_inquilino = nombre_inquilino;
+    }
+
+    public void setApellido_inquilino(String apellido_inquilino) {
+        this.apellido_inquilino = apellido_inquilino;
+    }
+
+    public Long getId_propiedad() {
+        return id_propiedad;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public Double getValor_alquiler() {
+        return valor_alquiler;
+    }
+
+    public String getNombre_inquilino() {
+        return nombre_inquilino;
+    }
+
+    public String getApellido_inquilino() {
+        return apellido_inquilino;
+    }
+
 }
